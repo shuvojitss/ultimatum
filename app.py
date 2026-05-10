c3f0a0e5ecf0e794599b651fc57dda91af80db40
Shuvojit Samanta <142198651+shuvojitss@users.noreply.github.com>
2026-05-10 12:40:49 +0000

    Enhance Flask application with environment variables and ConfigMap integration

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
 import socket
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     hostname = socket.gethostname()
-    return render_template('index.html', pod_name=hostname)
+
+    app_name = os.getenv("APP_NAME", "Default App")
+    message = os.getenv("WELCOME_MSG", "Hello World")
+
+    return render_template(
+        'index.html',
+        pod_name=hostname,
+        app_name=app_name,
+        message=message
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
